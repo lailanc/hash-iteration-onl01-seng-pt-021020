1862bea15d91fcf9318e44376feb20d85570399a
Laila Campbell <lailacampbell96@gmail.com>
2020-02-23 18:47:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,4 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-birthday_kids.each do |kids_name, age|
-  if birthday_kids>12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+
